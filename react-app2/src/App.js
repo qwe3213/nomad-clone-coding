bf90da8240f11ec671aa363b53345d34f428118a
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-06 23:24:27 +0900

    React Router

--- react-app2/src/App.js
+++ react-app2/src/App.js
@@ -1,38 +1,19 @@
-import {useState,useEffect} from "react";
-
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-      const json = await (
-        await fetch(
-          `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-        )
-      ).json();
-      setMovies(json.data.movies);
-      setLoading(false);
-    };
-    useEffect(() => {
-      fetch("https://api.coinpaprika.com/v1/tickers")
-        .then((response) => response.json())
-        .then((json) => {
-          setCoins(json);
-          setLoading(false);
-        });
-      getMovies();
-    }, []);
-   return  <div>{loading ? <h1>Loading...</h1> : <div>{movies.map(movie=><div key={movie.id}>
-        <img src={movie.medium_cover_image} />
-       <h2>{movie.title}</h2>
-       <p>{movie.summary}</p>
-       <ul>
-           <li>
-               {movie.genres.map(g=><li key={g }>{g}</li>)}
-           </li>
-       </ul>
-   </div>)}</div>}</div>;
+    return(<Router>
+        <Switch>
+          
+            <Route path="/movie/:id">
+                <Detail />
+            </Route>
+            <Route path="/">
+            <Home/>
+            </Route>
+        </Switch>
+    </Router>
+    );
 }
 export default App;
 
